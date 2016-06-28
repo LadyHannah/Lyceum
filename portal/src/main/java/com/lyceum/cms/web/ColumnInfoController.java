d167e4f37ad3c0855c99f896c337d53dd5e0d543
Han Cheng <Han Cheng>
2016-06-28 15:17:25 +0800

    change jdbc connection

--- portal/src/main/java/com/lyceum/cms/web/ColumnInfoController.java
+++ portal/src/main/java/com/lyceum/cms/web/ColumnInfoController.java
@@ -3,7 +3,6 @@ package com.lyceum.cms.web;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,7 +26,6 @@ import net.sf.json.JSONObject;
  */
 @Controller
 @RequestMapping("cms/column")
-//@EnableWebSecurity
 public class ColumnInfoController extends FreeMarkerController {
 
 	@Autowired
