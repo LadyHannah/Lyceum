38c32627b3cca5e11b09c20f360df0d60f778d33
hancheng <hancheng@swirebev.com>
2016-08-14 17:20:10 +0800

    Home page button link

---
+++ portal/src/main/webapp/static/js/common.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?a0d6cb05ef88dd0af57efe3e1ad29ff5";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
